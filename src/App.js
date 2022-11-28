3802bc52fc54ae7c2de59e534881848cae2a8ac3
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2022-11-28 19:57:22 +0100

    Added the GutHub Url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://heroic-sprinkles-834702.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Natsuki Shuko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/NatsukiSh/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
