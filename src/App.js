ea587b43839aa6a7ef0400418b0db24e7ccb7cad
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2022-11-28 20:23:32 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://heroic-sprinkles-834702.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Natsuki Shuko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/NatsukiSh/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://heroic-sprinkles-834702.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Natsuki Shuko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/NatsukiSh/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
