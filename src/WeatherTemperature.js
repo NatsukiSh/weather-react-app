712d6860eb536114026ae0dc5456f932f303c6fa
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2022-12-04 14:24:01 +0100

    Corrected spelling as lower case

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -9,7 +9,7 @@ export default function WeatherTemperature(props) {
 
   function showCelsius(event) {
     event.preventDefault();
-    setUnit("Celsius");
+    setUnit("celsius");
   }
 
   function fahrenheit() {
